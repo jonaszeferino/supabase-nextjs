c60329299468349f0e1efdbfe0d441db51d985f0
jonaszeferino <jonaszeferino@gmail.com>
2024-01-23 11:54:33 -0300

    new consoles.log on profile page with get method using querystring

--- pages/api/privateRoute.js
+++
@@ -1,7 +0,0 @@
-import requireAuth from '../../middleware/requireAuth';
-
-const handler = (req, res) => {
-  res.status(200).json({ message: 'Authenticated Route', user: req.user });
-};
-
-export default requireAuth(handler);
